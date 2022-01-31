ede8c422ec27e4cde2f6d6dcb5d83a2f9581aac1
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-02-01 00:35:41 +0100

    format

--- src/Code128.elm
+++ src/Code128.elm
@@ -231,7 +231,7 @@ encodeC : List Char -> Result Error (List Symbol)
 encodeC a =
     a
         |> toTuples
-        |> Result.fromMaybe (OddNumberOfDigits)
+        |> Result.fromMaybe OddNumberOfDigits
         |> Result.andThen
             (\v ->
                 v
